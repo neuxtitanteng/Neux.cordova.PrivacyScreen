e31417a1606caa5f85e95f2e1c5916aba25eee76
HsuanYuLin <kevinlin1168@gmail.com>
2020-02-19 21:33:15 +0800

    change android plugin

---
+++ src/android/PrivacyScreen.java
@@ -0,0 +1,31 @@
+package Neux.cordova.privacyscreen;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaWebView;
+
+import android.app.Activity;
+import android.view.Window;
+import android.view.WindowManager;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.Context;
+import android.os.Bundle;
+
+/**
+ * This class sets the FLAG_SECURE flag on the window to make the app
+ *  private when shown in the task switcher
+ */
+public class PrivacyScreenPlugin extends CordovaPlugin {
+
+  @Override
+  public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+    super.initialize(cordova, webView);
+    Activity activity = this.cordova.getActivity();
+    activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_SECURE);
+  }
+}
